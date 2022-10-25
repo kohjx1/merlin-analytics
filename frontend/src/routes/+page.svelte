8fc47b9b1f75d8a9b74155b3a0957ec650f3aa70
Teo Yun Lin <beatriceisworking@gmail.com>
2022-10-25 17:34:20 +0800

    Frontend Setup with sample charts

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import WordCloud from "../components/charts/WordCloud.svelte"
+  import DonutChart from "../components/charts/DonutChart.svelte"
+  import Grid from "svelte-grid-responsive"
+</script>
+
+<h2>Data Analytics</h2>
+
+<br/>
+
+<Grid container>
+  <Grid xs={8} md={8} lg={8}>
+    <WordCloud />
+  </Grid>
+  <Grid xs={4} md={4} lg={4}>
+    <DonutChart />
+  </Grid>
+</Grid>
